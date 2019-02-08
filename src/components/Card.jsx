4131c74a8f1870a848273adfeb667f7f2a713b76
Cook1619 <cook6348@gmail.com>
2019-02-08 10:09:55 -0600

    Card component added

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = props => {
+  return (
+      <div className="col-3 m-1 card bg-light">
+        <div className="card-body">
+          <div className="card-title">
+            {props.cardData.name} @{props.cardData.handle}
+          </div>
+          <div className="card-text">{props.cardData.body}</div>
+        </div>
+      </div>
+  );
+};
+
+export default Card;s
