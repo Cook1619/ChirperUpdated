08051cab351fdd0e3e5c44675dc1bde8aac9c5ae
Cook1619 <cook6348@gmail.com>
2019-02-08 12:31:44 -0600

    Updates

---
+++ precache-manifest.3d9be0e2787fcddaaeb2dae28bb7545d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a1f474daf91508a66b5",
+    "url": "/ChirperUpdated/static/js/runtime~main.8a1f474d.js"
+  },
+  {
+    "revision": "205269325dd9f79a0bbc",
+    "url": "/ChirperUpdated/static/js/main.20526932.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/ChirperUpdated/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "205269325dd9f79a0bbc",
+    "url": "/ChirperUpdated/static/css/main.28fc4e0b.chunk.css"
+  },
+  {
+    "revision": "bb7dcf9df570905cb5583ede8391543d",
+    "url": "/ChirperUpdated/index.html"
+  }
+];
