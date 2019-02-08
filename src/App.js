7e8a6cb1d0880080064cfde1c54540c6267dd079
Cook1619 <cook6348@gmail.com>
2019-02-08 10:08:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { Component } from "react";
+import "./App.css";
+import CardList from "./components/CardList";
+import ChirpSubmit from "./components/ChirpSubmit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chirps: [
+        {
+          name: "Matt",
+          handle: "cook1619",
+          body: "Chirp 1 Body"
+        },
+        {
+          name: "Danelle",
+          handle: "Playful1303",
+          body: "Chirp 2 Body"
+        },
+        {
+          name: "Jules",
+          handle: "Imcute14",
+          body: "Chirp 3 Body"
+        }
+      ]
+    };
+
+    this.handleClick = event => {
+      let newChirp = {
+        name: document.getElementById("inputNameField").value,
+        handle: document.getElementById("inputHandleField").value,
+        body: document.getElementById("inputBodyField").value
+      };
+      this.setState({
+        chirps: this.state.chirps.concat([newChirp])
+      });
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid m-2">
+        <div className="row">
+          <CardList cardArray={this.state.chirps} />
+          <ChirpSubmit handleClick={this.handleClick} />
+        </div>
       </div>
     );
   }
